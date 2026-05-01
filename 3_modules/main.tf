89bfb089e6e30c7035335a98bb392b2283ca9d5a
Nikhil Kumar <nikhilshaw575@gmail.com>
2026-05-01 12:19:18 +0530

    Add EC2 instance module and documentation for Terraform modules

---
+++ 3_modules/main.tf
@@ -0,0 +1,10 @@
+# provider "aws"{
+#     region = "us-east-1"
+# }
+
+# module "ec2_instance" {
+#   source = "../module/ec2_instance"
+#   ami_value="ami-091138d0f0d41ff90"
+#   instance_value="t2.micro"
+#   subnet_id_value="subnet-0d6c8221bc8759703"
+# }
--- EC2-creation/main.tf
+++ EC2-creation/main.tf
@@ -1,7 +1,19 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "example" {
+# resource "aws_instance" "example" {
+#   ami           = "ami-0ec10929233384c7f" #AMI ID
+#   instance_type = "t2.micro"
+#   subnet_id     = "subnet-0d6c8221bc8759703"
+#   key_name      = "AWS-Login"
+# }
+resource "aws_instance" "Server-2" {
+  ami           = "ami-0ec10929233384c7f" #AMI ID
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0d6c8221bc8759703"
+  key_name      = "AWS-Login"
+}
+resource "aws_instance" "Server-3" {
   ami           = "ami-0ec10929233384c7f" #AMI ID
   instance_type = "t2.micro"
   subnet_id     = "subnet-0d6c8221bc8759703"
